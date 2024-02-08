1baac4d87f841f02b463539551a90db3ca164dab
makeouthill6666 <superduperrookie@gmail.com>
2024-02-08 13:50:12 +0900

    v0.6 guess number

--- day01.py
+++ day01.py
@@ -1,9 +1,21 @@
-import mymath
+import random
 
-if __name__ == "__main__":
-    n = int(input("Input n : "))
-    r = int(input("Input r : "))
-    print(f"{n}C{r} = {mymath.nCr(n, r)}")
 
-    # f = int(input())
-    # print(factorial(f))
+target_number = random.randint(1, 100)
+chance = 7
+print("1-100")
+
+while chance != 0 :
+    guess_number = int(input("Input guess number : "))
+    if guess_number == target_number :
+        print("Correct")
+        break
+    elif guess_number > target_number :
+        chance = chance - 1
+        print(f"lower. {chance} chance left")
+    else :
+        chance = chance - 1
+        print(f"higher. {chance} chance left")
+
+else :
+    print ("no more chance")
