714081f5bcc72e3cd36961324f4a9f6a527e5dbf
makeouthill6666 <superduperrookie@gmail.com>
2024-02-08 10:26:47 +0900

    v0.2 combination

---
+++ day01.py
@@ -0,0 +1,23 @@
+def factorial(number) -> int :
+    result = 1
+    for i in range(1, number+1):
+        result = result * i
+    return result
+
+
+def nCr(n, r) -> int :
+    '''
+    조합함수
+    :param n:
+    :param r:
+    :return:
+    '''
+
+    numerator = factorial(n)
+    denominator = factorial(n-r) * factorial(r)
+    return int(numerator / denominator)
+
+if __name__ == "__main__":
+    n = int(input("Input n : "))
+    r = int(input("Input r : "))
+    print(f"{n}C{r} = {nCr(n, r)}")
