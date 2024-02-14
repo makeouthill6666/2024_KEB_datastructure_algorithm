dd5b08a8657aab7edfbfe776d5b9ae06e92ba2a3
makeouthill6666 <superduperrookie@gmail.com>
2024-02-14 14:16:46 +0900

    v1.3 dfs

---
+++ day03.py
@@ -0,0 +1,19 @@
+graph = [
+    [0, 1, 0, 1, 0],
+    [1, 0, 1, 1, 0],
+    [0, 1, 0, 0, 1],
+    [1, 1, 0, 0, 1],
+    [0, 0, 1, 1, 0],
+]
+
+def dfs(g, v, visited):
+    visited[v] = True
+    print(chr(ord('A')+v), end=' ')
+    for i in range(len(graph)):
+        if graph[v][i] == True and not visited[i]:
+            dfs(g, i, visited)
+
+
+
+visited = [False] * len(graph)
+dfs(graph, 0 , visited)
