e3dc894354ad282ee7095e3e9d3171bcc14f26a9
makeouthill6666 <superduperrookie@gmail.com>
2024-02-14 16:13:57 +0900

    v1.7 recursion

--- day03.py
+++ day03.py
@@ -4,11 +4,10 @@ def decimal_to_octal(number:int) -> str:
     :param number: integer (base dec)
     :return: string (base octal)
     """
-    octal = ' '
-    while number > 0:
-        octal = str(number % 8) + octal
-        number = number // 8
-    return octal
+    if number < 8:
+        return str(number)
+    else:
+        return decimal_to_octal(n//8) + str(n%8)
 
 n = int(input("Input decimal number : "))
 print(decimal_to_octal(n))
